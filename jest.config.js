02137f3ba104258a57598d450b22c39f0de99016
erichua(华挺) <erichua@webank.com>
2017-10-30 20:59:33 +0800

    add jest.config.js  support code coverage

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  notify: true,
+  verbose: true,
+  preset: "react-native",
+  bail: true, // stop @ the first fail
+  collectCoverage: true,
+  coverageDirectory: './__tests__/coverage/',
+  coverageReporters: ["lcov"],
+  testRegex: "./__tests__/.*\\-test\\.js$",
+  transformIgnorePatterns: [
+    "node_modules/(?!react-native|tcomb-form-native|react-native-localization|@exponent/react-native-action-sheet|@exponent/ex-navigation|@exponent/react-native-touchable-native-feedback-safe|rnrf-relay-renderer|react-clone-referenced-element|react-native-looped-carousel|rn-splash-screen)"
+  ],
+  transform: {
+    "^.+\\.(js)$": "<rootDir>/node_modules/webpack-babel-jest",
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
+  }
+}
